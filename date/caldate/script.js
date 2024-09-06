133f0713d191a6255b010439a626ff1158097ff7
Professor <sahiillx02@gmail.com>
2024-09-06 12:33:09 +0530

    calendar date picker

---
+++ date/caldate/script.js
@@ -0,0 +1,4 @@
+// CSS STYLES ARE INJECTED TO HEAD IN CODEPEN SETTINGS
+import { MonthPicker } from "https://cdn.skypack.dev/monthpicker-lite-js@1.2.0";
+const monthPicker = new MonthPicker(document.querySelector('.app'));
+monthPicker.open();
